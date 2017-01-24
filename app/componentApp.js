3a32ce3824d85e59641115bab814d5aab0beab18
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2017-01-24 13:07:49 -0500

    Created TransformPanel react component

--- app/componentApp.js
+++ app/componentApp.js
@@ -4,32 +4,11 @@ const {ipcRenderer} = require('electron')
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {ObjectPanel} from './panel'
-import {LabeledField} from './input'
+import {TransformPanel} from './componentPanel'
 
 ipcRenderer.on('setEntity', (event, data) => {
   ReactDOM.render(
-    <div className="component-panel">
-      <p>{data.name}</p>
-
-      <p>Position:</p>
-      <ObjectPanel
-        object={data.entity.transform.position}
-        onChange={(prop, val) => {}}
-      />
-
-      <LabeledField
-        label='Rotation'
-        value={data.entity.transform.rotation}
-        onChange={(val) => {}}
-      />
-
-      <p>Scale:</p>
-      <ObjectPanel
-        object={data.entity.transform.scale}
-        onChange={(prop, val) => {}}
-      />
-    </div>,
+    <ComponentPanel transform={data.entity.transform} />,
     document.getElementById('content')
   )
 })
