772b10d0849dabfc4b895c1c2ea59f0005d527f0
Ahnaf Siddiqui <luky1971@users.noreply.github.com>
2017-02-14 19:02:36 -0500

    Added close buttons to array

--- app/panel.js
+++ app/panel.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import {Button} from './button'
 import {LabeledField} from './input'
+import {copyObj} from './util'
+
+// TODO: recursively render arrays as well as objects
+// see http://stackoverflow.com/questions/767486/how-do-you-check-if-a-variable-is-an-array-in-javascript
 
 // prop onChange will be called when the value of a property input
 // in this panel changes value, passing this panel's label prop
@@ -52,11 +56,54 @@ export class ArrayPanel extends React.Component {
   render() {
     return (
       <div>
-        <ObjectPanel
-          label={this.props.label}
-          object={this.props.object}
-          onChange={this.props.onChange}
-        />
+        {!this.props.hideLabel && <h3>{this.props.label}</h3>}
+        {Object.keys(this.props.object).map(index => {
+          let xbutton = (
+            <div className="close-button-container">
+              <Button
+                content="x"
+                onClick={e => {
+                  let obj = this.props.object.slice(0)
+                  obj.splice(index, 1)
+                  this.props.onChange(this.props.label, obj)
+                }}
+              />
+            </div>
+          )
+
+          // recursively make object panel
+          if (typeof this.props.object[index] == 'object') {
+            return (
+              <div key={index}>
+                {xbutton}
+                <ObjectPanel
+                  label={index}
+                  object={this.props.object[index]}
+                  onChange={(label, val) => {
+                    let obj = this.props.object
+                    obj[label] = val
+                    this.props.onChange(this.props.label, obj)
+                  }}
+                />
+              </div>
+            )
+          }
+          // otherwise, this property is not an object
+          return (
+            <div key={index}>
+              {xbutton}
+              <LabeledField
+                label={index}
+                value={this.props.object[index]}
+                onChange={val => {
+                  let obj = this.props.object
+                  obj[index] = val
+                  this.props.onChange(this.props.label, obj)
+                }}
+              />
+            </div>
+          )
+        })}
         <div className="add-button-container">
           <Button content={this.props.addButtonContent} onClick={this.props.addItem} />
         </div>
